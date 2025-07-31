37d136b78750ce8ef8bebeb222d8e8a154c758b7
Yang Liu <liuyangzhuan@gmail.com>
2025-07-31 07:33:17 -0700

    weekly update

--- runit.sh
+++ runit.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
 
-. /opt/modules/default/etc/modules.sh
-module load python/3.7-anaconda-2019.10 
-cd /project/projectdirs/m2957/liuyangz/my_research/ghtraffic
+module load python
+cd /global/cfs/cdirs/m2957/liuyangz/my_research/ghtraffic
 git pull
 python ./ghtraffic.py
 git add *.org
